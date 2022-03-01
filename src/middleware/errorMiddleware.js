37bacf703f88e0c98fb012fc1a881bb861888491
nrj <admin_nrj@mail.ru>
2022-03-01 17:49:22 +0300

    added long processing imitation

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -1,7 +1,6 @@
 const ApiError = require("../exceptions/apiError");
 
 module.exports = function (err, req, res, next) {
-    console.log('error ',err);
     if (err instanceof ApiError){
         return res.status(err.status).json({message:err.message,errors:err.errors})
     }
--- src/service/paymentService.js
+++ src/service/paymentService.js
@@ -3,9 +3,15 @@ const PaymentDto = require('../dtos/paymentDto')
 
 class PaymentService {
     async payment(paymentData) {
-
+        //long processing imitation
+        await sleep(5000);
         return new PaymentDto(await dbService.addPayment(paymentData))
     }
 }
 
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
 module.exports = new PaymentService()
